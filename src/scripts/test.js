362b571abaedf488948edd7a9432f8e41f25174c
zH <shenzh2012@gmail.com>
2018-11-29 16:56:19 +0800

    [fix] miss export module from

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,7 +1,10 @@
 const { getRootPath } = require('../utils/pathHelper')
 const { runCLI } = require('jest-cli/build/cli')
+const path = require('path')
 
 const testRunner = ctx => {
   const rootPath = getRootPath(path.resolve())
   return runCLI({}, [rootPath])
 }
+
+module.exports = testRunner
